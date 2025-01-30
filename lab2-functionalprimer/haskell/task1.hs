2f165dd1513af04f29a72039d6fa18f3b6d609ed
KaiLj005 LJ <kailowe66@gmail.com>
2025-01-30 10:10:00 +0000

    change2

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -2,13 +2,11 @@ module Main where
 
 main =
     do
-    print (onePlusOne)
     print (onePlusOneEq 1)
     print (myFunc1 5)
     print (myFunc2 10)
     print (thirdFunc)
 
-onePlusOne = "1 + 1 = 2"
 onePlusOneEq x = x+1
 myFunc1 x = x*10
 myFunc2 x = x*2
--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,7 @@
 main = do
+    print (onePlusOne)
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+
+onePlusOne = "1 + 1 = 2"
